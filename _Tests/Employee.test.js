af0dea8ea05b2089a3ba694126c1fedb99caeaf5
Mohamed <abdi2k24@gmail.com>
2023-01-24 00:20:03 -0600

    added html

---
+++ _Tests/Employee.test.js
@@ -0,0 +1,12 @@
+const Employee = require('../lib/Employee')
+test("getRole() should return \"Employee\"", () => {
+    const ExpectedValue = "Employee";
+    const e = new Employee("Mohamed", 1, "abdi2k24@gmail.com");
+    expect(e.getRole()).toBe(expectedValue);
+  });,
+
+  test("getName() should return \"Mohamed\"", () => {
+    const expectedValue = "Mohamed";
+    const e = new Employee("Mohamed", 1, "Mohamed@gmail.com");
+    expect(e.getName()).toBe(expectedValue);
+  });,
